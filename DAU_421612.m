954808e0785575086274a16a68d2b7ee03af734e
Wahyu Widhi Dyatmika <146448699+wwdhd@users.noreply.github.com>
2023-12-05 03:12:48 +0000

    Number 5 extension
    Added analytical von Karman log Law of the Wall for number 5.

--- DAU_421612.m
+++ DAU_421612.m
@@ -1,13 +1,14 @@
-%Spatiotemporal Data Analysis
-%Adapted from read_data.m, generated form Kundu et. al, written by Dr. Tamas Jozsa
+%% Spatiotemporal Data Analysis
+%% Adapted from read_data.m, generated form Kundu et. al, written by Dr. Tamas Jozsa
+
 %This file is made in .mlx to create faster debugging
 
 clear all
 close all
 clc
+% READ CHANNEL PARAMETERS AND SPATIO-TEMPORAL AVERAGES
 
-%% Read Channel Parameters and Spatio-Temporal Averages
-
+% read parameters
 % Lz, Lx, Ly, nu, Delta p
 params = xlsread('Reynolds_stresses.xlsx','parameters');
 
@@ -59,31 +60,98 @@ legend('$u/u_b$','$v/u_b$','$w/u_b$',...
     'Interpreter','latex','FontSize',14,'Location','east')
 set(gca,'TickLabelInterpreter','latex','FontSize',14)
 hold off
-
-%% Mean
+% Mean and Standard Deviation at Every Sampling Point
 
 %u-velocity sampling
-u_means = mean(u_smpl, 1);
-u_stds = std(u_smpl, 0, 1);
+u_means = mean(u_smpl, 2);
+u_stds = std(u_smpl, 0, 2);
 
 %v-velocity sampling
-v_means = mean(v_smpl, 1);
-v_stds = std(v_smpl, 0, 1);
+v_means = mean(v_smpl, 2);
+v_stds = std(v_smpl, 0, 2);
 
 %w-velocity sampling
-w_means = mean(w_smpl, 1);
-w_stds = std(w_smpl, 0, 1);
+w_means = mean(w_smpl, 2);
+w_stds = std(w_smpl, 0, 2);
 
 %Plotting the Mean and STD
+%MEAN
 figure(2)
-plot(x_smpl,u_means,':r',LineWidth=2)
+plot(t_smpl,u_means,':r',LineWidth=2)
 hold on
-plot(x_smpl,v_means,'--b',LineWidth=2)
-plot(x_smpl,w_means,'-k',LineWidth=2)
+plot(t_smpl,v_means,'--b',LineWidth=2)
+plot(t_smpl,w_means,'-k',LineWidth=2)
 title('$\textbf{Mean at Every Sampling Point}$','Interpreter','latex','FontSize',14)
-xlabel('$x/\delta$','Interpreter','latex','FontSize',14)
-xlim([0,1])
-ylim([-0.2,1.4])
-legend('$u/u_b$','$v/u_b$','$w/u_b$','Interpreter','latex','FontSize',14,'Location','east')
+xlabel('$t$','Interpreter','latex','FontSize',14)
+legend('$u/u_b$','$v/u_b$','$w/u_b$',...
+    'Interpreter','latex','FontSize',14,'Location','east')
 set(gca,'TickLabelInterpreter','latex','FontSize',14)
 hold off
+
+%STD
+figure(3)
+plot(t_smpl,u_stds,':r',LineWidth=2)
+hold on
+plot(t_smpl,v_stds,'--b',LineWidth=2)
+plot(t_smpl,w_stds,'-k',LineWidth=2)
+title('$\textbf{Standard Deviation at Every Sampling Point}$','Interpreter','latex','FontSize',14)
+xlabel('$t$','Interpreter','latex','FontSize',14)
+legend('$u/u_b$','$v/u_b$','$w/u_b$',...
+    'Interpreter','latex','FontSize',14,'Location','east')
+set(gca,'TickLabelInterpreter','latex','FontSize',14)
+hold off
+
+%% Wall Normal Gradient of Mean Streamwise Velocity Component
+% Because the normal is mostly regarding the u component, it can be seen that 
+% u component is prominent compared to the two other components. 
+
+%u-velocity sampling
+u_wn = mean(u_smpl, 1);
+
+
+%v-velocity sampling
+v_wn = mean(v_smpl, 1);
+
+
+%w-velocity sampling
+w_wn = mean(w_smpl, 1);
+
+%Plotting the Mean and STD
+figure(4)
+plot(x_smpl,u_wn,':r',LineWidth=2)
+hold on
+plot(x_smpl,v_wn,'--b',LineWidth=2)
+plot(x_smpl,w_wn,'-k',LineWidth=2)
+title('$\textbf{Wall Normal Velocity Gradient}$','Interpreter','latex','FontSize',14)
+xlabel('$y$','Interpreter','latex','FontSize',14)
+xlim([0,1])
+ylim([-0.2,1.4])
+legend('$u/u_b$','$v/u_b$','$w/u_b$',...
+    'Interpreter','latex','FontSize',14,'Location','east')
+set(gca,'TickLabelInterpreter','latex','FontSize',14)
+hold off
+
+%% |Analytical|
+
+%Log law of the wall
+c_f = 0.026/(Re_b)^(1/7);
+tau_w = 0.5*c_f*rho*u_b^2;
+u_t = sqrt(tau_w/rho);
+
+%von-karman log velocity profile
+delta_v = nu/u_t;
+kappa = 0.41;
+y0 = kappa*delta_v;
+
+y = linspace(y0, 1, 100); % Wall-normal distance array
+
+% Calculate the mean streamwise velocity using the von Kármán logarithmic law
+U = (u_t / kappa) * log(y / y0);
+
+% Plotting the von Kármán logarithmic law profile
+figure(6);
+plot(y, U, 'b', 'LineWidth', 2);
+title('von Kármán Logarithmic Law of the Wall');
+xlabel('Mean Streamwise Velocity (U)');
+ylabel('Wall-normal Distance (y)');
+grid on;
