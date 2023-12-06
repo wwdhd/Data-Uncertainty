c049879127538ebcdf935679a6422a7dc0907ba0
Wahyu Widhi Dyatmika <146448699+wwdhd@users.noreply.github.com>
2023-12-06 02:50:41 +0000

    Added FFT Initialization
    Number 3 postponed, number 5 is started by initializing signals in x = 0.06

--- DAU_421612.m
+++ DAU_421612.m
@@ -1,12 +1,11 @@
-%% Spatiotemporal Data Analysis
-%% Adapted from read_data.m, generated form Kundu et. al, written by Dr. Tamas Jozsa
+%% |Spatio-Temporal Calculation|
+%% |Adapted from read_data.m, generated form Kundu et. al, written by Dr. Tamas Jozsa|
+% |Start|
 
-%This file is made in .mlx to create faster debugging
-
-clear all
+clear
 close all
 clc
-% READ CHANNEL PARAMETERS AND SPATIO-TEMPORAL AVERAGES
+% |READ CHANNEL PARAMETERS AND SPATIO-TEMPORAL AVERAGES|
 
 % read parameters
 % Lz, Lx, Ly, nu, Delta p
@@ -59,99 +58,135 @@ ylim([-0.2,1.4])
 legend('$u/u_b$','$v/u_b$','$w/u_b$',...
     'Interpreter','latex','FontSize',14,'Location','east')
 set(gca,'TickLabelInterpreter','latex','FontSize',14)
+grid on
 hold off
-% Mean and Standard Deviation at Every Sampling Point
+%% |Mean Streamwise Velocity Component|
+% |Because the normal is mostly regarding the u component, it can be seen that 
+% u component is prominent compared to the two other components.| 
 
 %u-velocity sampling
-u_means = mean(u_smpl, 2);
-u_stds = std(u_smpl, 0, 2);
+u_mean = mean(u_smpl, 1);
+u_std = std(u_smpl);
 
 %v-velocity sampling
-v_means = mean(v_smpl, 2);
-v_stds = std(v_smpl, 0, 2);
+v_mean = mean(v_smpl, 1);
+v_std = std(v_smpl);
+
 
 %w-velocity sampling
-w_means = mean(w_smpl, 2);
-w_stds = std(w_smpl, 0, 2);
+w_mean = mean(w_smpl, 1);
+w_std = std(w_smpl);
 
 %Plotting the Mean and STD
-%MEAN
-figure(2)
-plot(t_smpl,u_means,':r',LineWidth=2)
-hold on
-plot(t_smpl,v_means,'--b',LineWidth=2)
-plot(t_smpl,w_means,'-k',LineWidth=2)
-title('$\textbf{Mean at Every Sampling Point}$','Interpreter','latex','FontSize',14)
-xlabel('$t$','Interpreter','latex','FontSize',14)
-legend('$u/u_b$','$v/u_b$','$w/u_b$',...
-    'Interpreter','latex','FontSize',14,'Location','east')
-set(gca,'TickLabelInterpreter','latex','FontSize',14)
-hold off
-
-%STD
 figure(3)
-plot(t_smpl,u_stds,':r',LineWidth=2)
+plot(x_smpl,u_mean,':r',LineWidth=2)
 hold on
-plot(t_smpl,v_stds,'--b',LineWidth=2)
-plot(t_smpl,w_stds,'-k',LineWidth=2)
-title('$\textbf{Standard Deviation at Every Sampling Point}$','Interpreter','latex','FontSize',14)
-xlabel('$t$','Interpreter','latex','FontSize',14)
-legend('$u/u_b$','$v/u_b$','$w/u_b$',...
-    'Interpreter','latex','FontSize',14,'Location','east')
-set(gca,'TickLabelInterpreter','latex','FontSize',14)
-hold off
-
-%% Wall Normal Gradient of Mean Streamwise Velocity Component
-% Because the normal is mostly regarding the u component, it can be seen that 
-% u component is prominent compared to the two other components. 
-
-%u-velocity sampling
-u_wn = mean(u_smpl, 1);
-
-
-%v-velocity sampling
-v_wn = mean(v_smpl, 1);
-
-
-%w-velocity sampling
-w_wn = mean(w_smpl, 1);
-
-%Plotting the Mean and STD
-figure(4)
-plot(x_smpl,u_wn,':r',LineWidth=2)
-hold on
-plot(x_smpl,v_wn,'--b',LineWidth=2)
-plot(x_smpl,w_wn,'-k',LineWidth=2)
-title('$\textbf{Wall Normal Velocity Gradient}$','Interpreter','latex','FontSize',14)
+plot(x_smpl,v_mean,'--b',LineWidth=2)
+plot(x_smpl,w_mean,'-k',LineWidth=2)
+title('$\textbf{Mean Velocity at Every Sampling Point}$','Interpreter','latex','FontSize',14)
 xlabel('$y$','Interpreter','latex','FontSize',14)
 xlim([0,1])
 ylim([-0.2,1.4])
 legend('$u/u_b$','$v/u_b$','$w/u_b$',...
     'Interpreter','latex','FontSize',14,'Location','east')
 set(gca,'TickLabelInterpreter','latex','FontSize',14)
+grid on
 hold off
 
-%% |Analytical|
+%STD
+figure(4)
+plot(x_smpl,u_std,':r',LineWidth=2)
+hold on
+plot(x_smpl,v_std,'--b',LineWidth=2)
+plot(x_smpl,w_std,'-k',LineWidth=2)
+title('$\textbf{Standard Deviation at Every Sampling Point}$','Interpreter','latex','FontSize',14)
+xlabel('$t$','Interpreter','latex','FontSize',14)
+legend('$u/u_b$','$v/u_b$','$w/u_b$',...
+    'Interpreter','latex','FontSize',14,'Location','east')
+set(gca,'TickLabelInterpreter','latex','FontSize',14)
+grid on
+hold off
 
-%Log law of the wall
+%% |Wall Normal Streamwise Velocity Gradient|
+% |At this case, only w/ub is the only velocity component that has values in 
+% wall-normal direction. The other two has near zero value and thus can be ignored.|
+
+w_grad = gradient(w_mean);
+figure(5)
+figure(5)
+plot(x_smpl, w_grad, '-k', 'LineWidth', 2)
+title('$\textbf{Mean Streamwise Velocity Gradient}$', 'Interpreter', 'latex', 'FontSize', 14)
+xlabel('$t$', 'Interpreter', 'latex', 'FontSize', 14)
+legend('$w/u_b$', 'Interpreter', 'latex', 'FontSize', 14, 'Location', 'east')
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 14)
+
+% Get the handle to the title object
+title_handle = title('$\textbf{Mean Streamwise Velocity Gradient}$', 'Interpreter', 'latex', 'FontSize', 14);
+set(title_handle, 'Position', get(title_handle, 'Position') + [0 0.01 0]); % Adjust the vertical position
+grid on
+hold off
+%% |Analytical|
+% |To capture the nearly identical profile to the numerical result, von Kármán 
+% log velocity profile is used instead of the usual elliptical profile.| 
+
+%von Kármán Log Velocity Profile
+N = 5; %Number of Points
 c_f = 0.026/(Re_b)^(1/7);
 tau_w = 0.5*c_f*rho*u_b^2;
 u_t = sqrt(tau_w/rho);
-
-%von-karman log velocity profile
 delta_v = nu/u_t;
 kappa = 0.41;
 y0 = kappa*delta_v;
 
-y = linspace(y0, 1, 100); % Wall-normal distance array
+syms y
+y = linspace(y0, 1, N); % Wall-normal distance array
 
 % Calculate the mean streamwise velocity using the von Kármán logarithmic law
-U = (u_t / kappa) * log(y / y0);
+w_a = (u_t / kappa) * log(y / y0);
 
 % Plotting the von Kármán logarithmic law profile
 figure(6);
-plot(y, U, 'b', 'LineWidth', 2);
+plot(y, w_a, '-o', 'LineWidth', 2);
 title('von Kármán Logarithmic Law of the Wall');
-xlabel('Mean Streamwise Velocity (U)');
-ylabel('Wall-normal Distance (y)');
-grid on;
+xlabel('$x/\delta$','Interpreter','latex','FontSize',14);
+grid on
+hold off
+% |FFT Transformation at x = 0.06 location|
+% |Because of the value of the numerical solution array has no exact x = 0.06 
+% value, the index of which the value is the closest needs to be determined prior 
+% to the calculation| 
+
+%Finding the position of x = 0.06 in the x_smpl array
+specified_value = 0.06;
+
+% Find the index of the element in the array closest to the specified value
+[~, index] = min(abs(x_smpl - specified_value));
+
+% Display the index and the value in the array that is closest to the specified value
+closest_value = x_smpl(index);
+disp(['Index of closest element: ', num2str(index)]);
+disp(['Value in the array closest to ', num2str(specified_value), ': ', num2str(closest_value)]);
+
+%Plot the noisy signal with time
+u_int_area = u_smpl(:, index);
+v_int_area = v_smpl(:, index);
+w_int_area = w_smpl(:, index);
+
+figure(7)
+title('$\textbf{Wall Normal Velocity Signal}$','Interpreter','latex','FontSize',14)
+plot(t_smpl,u_int_area,':r',LineWidth=2)
+hold on
+plot(t_smpl,v_int_area,'--b',LineWidth=2)
+plot(t_smpl,w_int_area,'-k',LineWidth=2)
+xlabel('$x/\delta$','Interpreter','latex','FontSize',14)
+legend('$u/u_b$','$v/u_b$','$w/u_b$',...
+    'Interpreter','latex','FontSize',14,'Location','east')
+set(gca,'TickLabelInterpreter','latex','FontSize',14)
+grid on
+hold off
+%% 
+% |Then the FFT transformation can be made.|
+
+
+%% 
+%
